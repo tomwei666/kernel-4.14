ba1c92bb7b1a100e9c563aaa0ed129a88a712211
tom-wei <tomwei666@gmail.com>
2021-02-20 09:07:51 +0800

    [thread_info] 1. 从sp_el1得到stack. 2. 从sp_el0得到 task_struct,再从task_struct得到stack .

--- drivers/thread-pid/create_thread.c
+++ drivers/thread-pid/create_thread.c
@@ -2,12 +2,39 @@
 #include <linux/init.h>
 #include <asm/sysreg.h>
 #include <linux/sched/task.h>
+#include <asm/thread_info.h>
 
-/*pid_t kernel_thread(int (*fn)(void *), void *arg, unsigned long flags)*/
+
+register unsigned long current_stack_pointer asm ("sp");
+static inline struct task_struct *current_task_spel1(void) __attribute_const__;
+static inline struct task_struct *current_task_spel1(void)
+{
+	void *stack1 =(void *)(current_stack_pointer & ~(THREAD_SIZE - 1));
+	struct task_struct * task_tmp = container_of(stack1, struct task_struct, stack);
+	return task_tmp;
+}
+static inline void *current_thread_info2(void)
+{
+    return (void *)
+        (current_stack_pointer & ~(THREAD_SIZE - 1));
+}
 
 int kthread_fn(void *unsed)
 {
+	/*1. get task_struct from current function*/
 	printk(KERN_ERR "tom F=%s L=%d current->pid=%d\n",__func__,__LINE__,current->pid);
+	/*struct thread_info *ti = current_thread_info();*/
+
+	/*2. get task_struct from sp_el1 stack*/
+	struct task_struct * task_tmp = current_task_spel1();
+	printk(KERN_ERR "tom F=%s L=%d pid from sp_el1=%d\n",__func__,__LINE__,task_tmp->pid);
+
+	/*3. get stack from two method: 1.current->stac(from sp_el0) 2. from sp_el1*/
+	printk(KERN_ERR "tom F=%s L=%d current->stack=%llx\n",__func__,__LINE__,current->stack);
+	printk(KERN_ERR "tom F=%s L=%d current->stack=%llx\n",__func__,__LINE__,(current_stack_pointer & ~(THREAD_SIZE - 1)));
+	printk(KERN_ERR "tom F=%s L=%d current->stack=%llx\n",__func__,__LINE__,task_tmp->stack);
+
+	
 	return 0;
 }
 static int __init thread_pid_init(void)
